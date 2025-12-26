b217ab46ce2f59ac502906f71ef411ee2eb0b1be
Shubham700692 <shubhamkhanzode151@gmail.com>
2025-12-26 21:15:24 +0530

    cors setup in backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,24 +13,34 @@ const app = express();
 const server = createServer(app);
 const io = connectToSocket(server);
 
-app.get("/home",(req,res) =>{
-    return res.send("xxx");
-})
 
-app.set("port", (process.env.PORT || 8000))
-app.use(cors());
+
+// app.set("port", (process.env.PORT || 8000))
+const PORT = process.env.PORT || 8000;
+
+// app.use(cors());
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
 
 app.use("/api/v1/users", userRoutes);
+
 
 const start = async () => {
     app.set("mongo_user")
     const connectionDb = await mongoose.connect("mongodb+srv://zoom_user:zoom_password@cluster0.mi9xskf.mongodb.net/?appName=Cluster0")
 
     console.log(`MONGO Connected DB HOst: ${connectionDb.connection.host}`)
-    server.listen(app.get("port"), () => {
-        console.log("LISTENING ON PORT 8000")
+    // server.listen(app.get("port"), () => {
+    //     console.log("LISTENING ON PORT 8000")
+    server.listen(PORT, () => {
+    console.log(`LISTENING ON PORT ${PORT}`);
+
+
     });
 
 }
