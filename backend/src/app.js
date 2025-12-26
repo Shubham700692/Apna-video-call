c47025f722a1ab81b1b8db2a73c263813a1746d4
Shubham700692 <shubhamkhanzode151@gmail.com>
2025-12-26 18:30:44 +0530

    ready for deployment

---
+++ backend/src/app.js
@@ -0,0 +1,40 @@
+import express from "express";
+import { createServer } from "node:http";
+
+import { Server } from "socket.io";
+
+import mongoose from "mongoose";
+import { connectToSocket } from "./controllers/socketManager.js";
+
+import cors from "cors";
+import userRoutes from "./routes/users.routes.js";
+
+const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
+
+app.get("/home",(req,res) =>{
+    return res.send("xxx");
+})
+
+app.set("port", (process.env.PORT || 8000))
+app.use(cors());
+app.use(express.json({ limit: "40kb" }));
+app.use(express.urlencoded({ limit: "40kb", extended: true }));
+
+app.use("/api/v1/users", userRoutes);
+
+const start = async () => {
+    app.set("mongo_user")
+    const connectionDb = await mongoose.connect("mongodb+srv://zoom_user:zoom_password@cluster0.mi9xskf.mongodb.net/?appName=Cluster0")
+
+    console.log(`MONGO Connected DB HOst: ${connectionDb.connection.host}`)
+    server.listen(app.get("port"), () => {
+        console.log("LISTENING ON PORT 8000")
+    });
+
+}
+
+
+
+start();
